979787d60ee8377497513c349957da31c54e4b49
Júnior Bocelli <juniorbocelli@gmail.com>
2021-12-24 18:07:37 -0300

    Created basic post list component;

---
+++ client/src/screens/Blog/components/Posts/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { IUseStates } from "../../states";
+
+const Posts: React.FC<IUseStates> = (states) => {
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  );
+};
+
+export default Posts;
